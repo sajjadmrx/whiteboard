4014f979d3ff8f916e4ed182c2bfe0246a7961b7
sajjadmrx <sajjadzibaee102@gmail.com>
2022-01-19 21:14:59 +0330

    drawing

--- main.ts
+++ main.ts
@@ -9,18 +9,30 @@ const PORT = process.env.PORT || 3000;
 const app: express.Application = express();
 
 const server = http.createServer(app);
+const io = new Server(server);
+
+
+// interfaces
+interface IDrawingObject {
+  x0: number;
+  y0: number;
+  x1: number;
+  y1: number;
+  color: string;
+  username: string;
+}
+
 
 
 
 app.use(express.static(__dirname + '/public'));
-
-const io = new Server(server);
-
-
-
 io.on('connection', (socket: Socket) => {
   console.log('client connected');
 
+  socket.on('drawing', (data: IDrawingObject) => {
+    console.log(data);
+    socket.broadcast.emit('drawing', data);
+  })
 
   socket.on('disconnect', () => {
     console.log('Client disconnected');
